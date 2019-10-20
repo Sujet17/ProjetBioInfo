280f1e18d20efea98f0bed61cdfb4f5daab2d823
Sujet17 <joachim.sneessens@student.umons.ac.be>
2019-10-20 15:56:45 +0200

    Premier commit

---
+++ src/main/Projet.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Projet {
+	
+    // Here is an example of how to use the calculator:
+    public static void main(String args[])
+    {
+    	System.out.println("Ca compile");
+    }
+	
+}
